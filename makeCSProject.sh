b200414675ca79aa37c19b1b0584c47fa47fb103
Jaylyn Lowes <117837026+Jaylynlowes04@users.noreply.github.com>
2025-01-29 16:29:36 -0500

    Delete makeCSProject.sh

--- makeCSProject.sh
+++
@@ -1,7 +0,0 @@
-dotnet new sln
-dotnet new gitignore
-dotnet new classlib -o ${1}
-dotnet sln add "./${1}/${1}.csproj"
-dotnet new xunit -o ${1}.Tests
-dotnet sln add "./${1}.Tests/${1}.Tests.csproj"
-dotnet add "./${1}.Tests/{1}.Tests.csproj" reference "./${1}/${1}.csproj"
