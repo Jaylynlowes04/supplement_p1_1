f9767c929ba92039d6384bdfd3d545c812a909db
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-01-29 15:38:01 -0500

    chore: declared methods for area calculations

--- Supplement_p1_1/Supplement_p1_1.cs
+++ Supplement_p1_1/Supplement_p1_1.cs
@@ -2,5 +2,16 @@
 
 public class Supplement_p1_1
 {
+    public static double AreaOfRectangle(double length, double width){
+        throw new NotImplementedException();
+    }
+
+    public static double AreaOfCircle(double radius){
+        throw new NotImplementedException();
+    }
+
+    public static double AreaOfTriangle(double triangleBase, double triangleHeight){
+        throw new NotImplementedException();
+    }
 
 }
