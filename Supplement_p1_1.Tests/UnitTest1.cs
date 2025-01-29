a144b3d4e8076e666325c96e4abc410b7840f2da
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-01-29 16:02:15 -0500

    test: AreaOfTriangle test for random triangle

--- Supplement_p1_1.Tests/UnitTest1.cs
+++ Supplement_p1_1.Tests/UnitTest1.cs
@@ -13,9 +13,16 @@ public class UnitTest1
     {
         Assert.True(25.0 == Supplement_p1_1.AreaOfRectangle(5.0,5.0));
     }
+
     [Fact]
     public void ShouldCalculateAreaOfUnitCircle()
     {
         Assert.True(double.Pi == Supplement_p1_1.AreaOfCircle(1.0));
+    }
+    
+    [Fact]
+    public void ShouldCalculateAreaOfATriangle()
+    {
+        Assert.True(100 == Supplement_p1_1.AreaOfTriangle(50.0, 4.0));
     }
 }
