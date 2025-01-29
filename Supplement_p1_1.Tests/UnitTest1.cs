bfdae3b1da1a16bc7d42c8084f0f01d66037f527
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-01-29 15:44:37 -0500

    test: added test for rectangle area with same length and width

--- Supplement_p1_1.Tests/UnitTest1.cs
+++ Supplement_p1_1.Tests/UnitTest1.cs
@@ -7,4 +7,10 @@ public class UnitTest1
     {
         Assert.True(50.0 == Supplement_p1_1.AreaOfRectangle(5.0,10.0));
     }
+
+    [Fact]
+    public void ShouldCalculateAreaOfRectangleWithSameLengthAndWidth()
+    {
+        Assert.True(25.0 == Supplement_p1_1.AreaOfRectangle(5.0,5.0));
+    }
 }
